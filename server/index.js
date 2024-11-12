2fe4d7f7c278baea66a6cf8c0bbc19874037ceb8
Ayesha Khan <ayesha_2101ph12@iitp.ac.in>
2024-11-12 17:23:48 +0530

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -1,215 +1,235 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
 
-const server =  http.createServer(app);
+const server = http.createServer(app);
 const { Server } = require("socket.io");
-const {PeerServer} = require('peer');
+const { PeerServer } = require("peer");
 
+dotenv.config();
 app.use(cors());
 app.use(express.json()); //middleware
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "front", "build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "front", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+}
+
 const io = new Server(server, {
-    cors : {
-        origin: 'https://geo-call-main-client.vercel.app',
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get('/api', (req, res) => {
-    res.send('API is working!');
-  });  
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/api", (req, res) => {
+  res.send("API is working!");
 });
 
 let onlineUsers = {};
 let videoRooms = {};
 
-io.on('connection', (socket) => {             //emit an event 
-    console.log(`a user connected ${socket.id}`);
-    socket.on('user-login', (data) => loginEventHandler(socket, data));
+io.on("connection", (socket) => {
+  //emit an event
+  console.log(`a user connected ${socket.id}`);
+  socket.on("user-login", (data) => loginEventHandler(socket, data));
 
-    socket.on('chat-message', data => {
-        chatMessageHandler(socket,data);
-    })
+  socket.on("chat-message", (data) => {
+    chatMessageHandler(socket, data);
+  });
 
-    socket.on('video-room-create' , data => videoRoomCreateHandler(socket,data));
+  socket.on("video-room-create", (data) =>
+    videoRoomCreateHandler(socket, data)
+  );
 
-    socket.on('video-room-join', data =>{
-        videoRoomJoinHandler(socket,data);
-    })
+  socket.on("video-room-join", (data) => {
+    videoRoomJoinHandler(socket, data);
+  });
 
-    socket.on('video-room-leave', (data)=> {
-        videoRoomLeaveHandler(socket,data);
-    })
+  socket.on("video-room-leave", (data) => {
+    videoRoomLeaveHandler(socket, data);
+  });
 
-    socket.on('disconnect', () => {
-        disconnectEventHandler(socket.id);
-    })
+  socket.on("disconnect", () => {
+    disconnectEventHandler(socket.id);
+  });
 });
 
-const peerServer = PeerServer({port : 9000, path: '/peer'});
+const peerServer = PeerServer({ port: 9000, path: "/peer" });
 
 const PORT = process.env.PORT || 3003;
 
 server.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
 
 //socket events
 
 const loginEventHandler = (socket, data) => {
+  socket.join("logged-users"); // all logged-in user will join the room named as logged-users
 
-    socket.join("logged-users");  // all logged-in user will join the room named as logged-users
+  onlineUsers[socket.id] = {
+    username: data.username,
+    coords: data.coords,
+  };
 
-    onlineUsers[socket.id] = {
-        username: data.username,
-        coords: data.coords,
-    };
+  //console.log(onlineUsers);
 
-    //console.log(onlineUsers);
-
-    io.to('logged-users').emit("online-users", convertOnlineUsersToArray());
-    broadcastVideoRooms();
-    
+  io.to("logged-users").emit("online-users", convertOnlineUsersToArray());
+  broadcastVideoRooms();
 };
 
-const disconnectEventHandler = (id) =>{
-    console.log(`User Disconnected ${id}`);
-    checkIfUserIsInCall(id);
-    removeOnlineUser(id);
+const disconnectEventHandler = (id) => {
+  console.log(`User Disconnected ${id}`);
+  checkIfUserIsInCall(id);
+  removeOnlineUser(id);
 };
 
 const chatMessageHandler = (socket, data) => {
-    const {receiverSocketId, content, id} = data;
+  const { receiverSocketId, content, id } = data;
 
-    if(onlineUsers[receiverSocketId]){
-        console.log('message received');
-        console.log('sending message to other user');
-        io.to(receiverSocketId).emit('chat-message',{
-            senderSocketId: socket.id,
-            content: content,
-            id: id,
-        });
-    }
-}   
+  if (onlineUsers[receiverSocketId]) {
+    console.log("message received");
+    console.log("sending message to other user");
+    io.to(receiverSocketId).emit("chat-message", {
+      senderSocketId: socket.id,
+      content: content,
+      id: id,
+    });
+  }
+};
 
-const videoRoomCreateHandler = (socket,data) => {
-    const {peerId, newRoomId} = data;
+const videoRoomCreateHandler = (socket, data) => {
+  const { peerId, newRoomId } = data;
 
-    videoRooms[newRoomId] = {
-        participants: [
-            {
-                socketId: socket.id,
-                username: onlineUsers[socket.id].username,
-                peerId,
-            },
-        ],
-    };
+  videoRooms[newRoomId] = {
+    participants: [
+      {
+        socketId: socket.id,
+        username: onlineUsers[socket.id].username,
+        peerId,
+      },
+    ],
+  };
 
-    broadcastVideoRooms();
+  broadcastVideoRooms();
 
-    console.log("new room", data);
-
-}
+  console.log("new room", data);
+};
 const videoRoomJoinHandler = (socket, data) => {
-    const { roomId,peerId} = data;
+  const { roomId, peerId } = data;
 
-    if(videoRooms[roomId]){
-        videoRooms[roomId].participants.forEach((participant) =>{
-            socket.to(participant.socketId).emit('video-room-init',{
-                newParticipantPeerId: peerId,
-            });
-        });
+  if (videoRooms[roomId]) {
+    videoRooms[roomId].participants.forEach((participant) => {
+      socket.to(participant.socketId).emit("video-room-init", {
+        newParticipantPeerId: peerId,
+      });
+    });
 
-        videoRooms[roomId].participants = [
-            ...videoRooms[roomId].participants,
-            {
-                socketId: socket.id,
-                username: onlineUsers[socket.id].username,
-                peerId,
-            }
-        ];
-
-        broadcastVideoRooms();
-    }
-}
-
-const videoRoomLeaveHandler = (socket, data) =>{
-    const {roomId} = data;
-    if(videoRooms[roomId]){
-        videoRooms[roomId].participants = videoRooms[roomId].participants.filter(p => p.socketId !== socket.id);
-    };
-    //console.log(videoRooms[roomId]);
-
-    if(videoRooms[roomId].participants.length>0){
-        //emit an event to the user which is in the room that he should also close his peer connection
-        socket.to(videoRooms[roomId].participants[0].socketId).emit("video-call-disconnect");
-    }
-
-    if(videoRooms[roomId].participants.length<1){
-        delete videoRooms[roomId];
-    }
+    videoRooms[roomId].participants = [
+      ...videoRooms[roomId].participants,
+      {
+        socketId: socket.id,
+        username: onlineUsers[socket.id].username,
+        peerId,
+      },
+    ];
 
     broadcastVideoRooms();
-}
+  }
+};
+
+const videoRoomLeaveHandler = (socket, data) => {
+  const { roomId } = data;
+  if (videoRooms[roomId]) {
+    videoRooms[roomId].participants = videoRooms[roomId].participants.filter(
+      (p) => p.socketId !== socket.id
+    );
+  }
+  //console.log(videoRooms[roomId]);
+
+  if (videoRooms[roomId].participants.length > 0) {
+    //emit an event to the user which is in the room that he should also close his peer connection
+    socket
+      .to(videoRooms[roomId].participants[0].socketId)
+      .emit("video-call-disconnect");
+  }
+
+  if (videoRooms[roomId].participants.length < 1) {
+    delete videoRooms[roomId];
+  }
+
+  broadcastVideoRooms();
+};
 
 //helper functions
 const removeOnlineUser = (id) => {
-    if(onlineUsers[id]){
-        delete onlineUsers[id];
-    };
-    broadDisconnectedUserDetails(id);
-    console.log(onlineUsers);
+  if (onlineUsers[id]) {
+    delete onlineUsers[id];
+  }
+  broadDisconnectedUserDetails(id);
+  console.log(onlineUsers);
 };
 
 const checkIfUserIsInCall = (id) => {
-    Object.entries(videoRooms).forEach(([key,value]) => {
-        const participant = value.participants.find( p => p.socketId===id);
+  Object.entries(videoRooms).forEach(([key, value]) => {
+    const participant = value.participants.find((p) => p.socketId === id);
 
-        if(participant){
-            removeUserFromTheVideoRoom(id ,key);
-        }
-    })
+    if (participant) {
+      removeUserFromTheVideoRoom(id, key);
+    }
+  });
 };
 
-const removeUserFromTheVideoRoom =  (socketId,roomId) => {
-    videoRooms[roomId].participants = videoRooms[roomId].participants.filter(p => p.socketId !==socketId);
+const removeUserFromTheVideoRoom = (socketId, roomId) => {
+  videoRooms[roomId].participants = videoRooms[roomId].participants.filter(
+    (p) => p.socketId !== socketId
+  );
 
-    if(videoRooms[roomId].participants.length>0){
-        //emit an event to the user which is in the room that he should also close his peer connection
-        io.to(videoRooms[roomId].participants[0].socketId).emit("video-call-disconnect");
-    }
+  if (videoRooms[roomId].participants.length > 0) {
+    //emit an event to the user which is in the room that he should also close his peer connection
+    io.to(videoRooms[roomId].participants[0].socketId).emit(
+      "video-call-disconnect"
+    );
+  }
 
-    if(videoRooms[roomId].participants.length<1){
-        delete videoRooms[roomId];
-    }
+  if (videoRooms[roomId].participants.length < 1) {
+    delete videoRooms[roomId];
+  }
 
-    broadcastVideoRooms();
-
-
+  broadcastVideoRooms();
 };
 
-const broadDisconnectedUserDetails = (disconnectedUserSocketId) =>{
-    io.to('logged-users').emit('user-disconnected', disconnectedUserSocketId);
+const broadDisconnectedUserDetails = (disconnectedUserSocketId) => {
+  io.to("logged-users").emit("user-disconnected", disconnectedUserSocketId);
 };
 
 const broadcastVideoRooms = () => {
-    io.to('logged-users').emit('video-rooms', videoRooms);
-}
+  io.to("logged-users").emit("video-rooms", videoRooms);
+};
 
 const convertOnlineUsersToArray = () => {
-    const onlineUsersArray = [];
+  const onlineUsersArray = [];
 
-    Object.entries(onlineUsers).forEach(([key,value]) => {
-        onlineUsersArray.push({
-            socketId: key,
-            username: value.username,
-            coords: value.coords,
-        });
+  Object.entries(onlineUsers).forEach(([key, value]) => {
+    onlineUsersArray.push({
+      socketId: key,
+      username: value.username,
+      coords: value.coords,
     });
+  });
 
-    return onlineUsersArray;
+  return onlineUsersArray;
 };
